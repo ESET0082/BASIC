4fb9afb2abfa1bea095880dc35c7788aa898e63c
ESET0082 <gopalbhai.bhammar@esyasoft.com>
2025-09-04 22:44:08 +0530

    add all folder

---
+++ ReactJS/Day-4/my-project-vite/src/counter.jsx
@@ -0,0 +1,17 @@
+function Countes()
+{
+    let count=0;
+    const increment=()=>{
+        count++;
+        document.getElementById("c1").innerText=count;
+    }
+    return (
+        <>
+            <h1 id="c1">0</h1>
+            <button onClick={increment}>INC</button>
+        </>
+    )
+}
+export default Countes;
+
+
