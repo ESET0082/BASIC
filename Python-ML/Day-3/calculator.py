60614def689fd7ddf335f265dc9359bdc5dee8fe
ESET0082 <gopalbhai.bhammar@esyasoft.com>
2025-09-05 11:48:18 +0530

    add python files in python-ML folder

---
+++ Python-ML/Day-3/calculator.py
@@ -0,0 +1,24 @@
+def add(a,b):   # add two numbwr
+    return a+b
+
+def sub(a,b):   # subtract two number
+    return a-b
+
+def mul(a,b):   # multiple two number
+    return a*b
+
+def div(a,b):   # division operator
+    if b==0:
+        return 0
+    return a/b
+
+'''
+print(add(5,6))
+
+print(sub(5,6))
+
+print(mul(5,6))
+
+print(div(5,6))'''
+
+    
---
+++ Python-ML/Day-3/module.py
@@ -0,0 +1,19 @@
+from calculator  import add  # add specific function from calculator module
+
+import calculator as cal  # add new module calculater(all function) fron other file
+
+import random
+import math
+import itertools
+import datetime
+
+print(add(2,1000))
+
+int(cal.add(5,6))
+
+print(cal.sub(5,6))
+
+print(cal.mul(5,6))
+
+print(cal.div(5,6))
+
