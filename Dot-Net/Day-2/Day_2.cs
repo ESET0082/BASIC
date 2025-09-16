565da45ae2c8ae0977b95bee5c01616d20241ffa
ESET0082 <gopalbhai.bhammar@esyasoft.com>
2025-09-16 09:47:40 +0530

    add day-2

--- Dot-Net/Day-2/Day_2.cs
+++ Dot-Net/Day-2/Day_2.cs
@@ -5,41 +5,19 @@
         static int sum;
         static void Main(string[] args)
         {
-            int num1, num2, number_student;
-            Console.WriteLine("enter number of subject!:-");  //output command like printf,cout
-            number_student = Convert.ToInt32(Console.ReadLine()); //input command like scanf,cin(user input)
-            int i = 0, Total = 0;
-            int[] Strudent_mark = new int[number_student];
-            while (i < number_student)
-            {
-                Console.WriteLine("Enter marks for subject " + (i + 1) + ": ");
-                Strudent_mark[i] = Convert.ToInt32(Console.ReadLine());
-                Total += Strudent_mark[i];
-                i++;
-            }
-            while (true)
-            {
-                int choice;
-                Console.WriteLine("enter your choice");
-                Console.WriteLine("total sum of all subjext press 1 :");
-                Console.WriteLine("average of all subject press 2 :");
-                Console.WriteLine("Exit press any other key :");
-                choice = Convert.ToInt32(Console.ReadLine());
-                if (choice == 1)
-                {
-                    Console.WriteLine("total of all subject: " + Total);
-                }
-                else if (choice == 2)
-                {
-                    float avg = Total / number_student;
-                    Console.WriteLine("average of all subject: ", +avg);
-                }
-                else
-                {
-                    break;
-                }
-            }
-
+            int num1, num2;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter first number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            sum = num1 + num2;
+            Console.WriteLine("The sum is: " + sum);
+            var myResult = Addition(num1, num2);
+            string name = "abcd";
+            string displayName = name ?? "Unknown";
+            Console.WriteLine(displayName); // Output: Unknown
+            Console.WriteLine(myResult.firstnumber); // Output: Unknown
         }
         static (int firstnumber, int secondnumber, int result) Addition(int firstnumber, int secondnumber)
         {
