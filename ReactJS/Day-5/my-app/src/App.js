265401f323ebf017c94d2ef8111689477dd4aa0b
ESET0082 <gopalbhai.bhammar@esyasoft.com>
2025-09-17 15:37:08 +0530

    add react files

---
+++ ReactJS/Day-5/my-app/src/App.js
@@ -0,0 +1,26 @@
+
+import { useState } from "react";
+
+
+
+function App(props) {
+
+  const [count, setCount] = useState(0);
+  const increment=()=>{
+    console.log("Increment Clicked");
+    setCount(count+1);
+  }
+  const decrement=()=>{
+    console.log("Decrement Clicked");
+    setCount(count-1);
+  }
+  return (
+   <div>
+    <h1>Count: {count}</h1>
+    <button onClick={increment}>Increment</button>
+    <button onClick={decrement}>decrement</button>
+   </div>
+  );
+}
+
+export default App;
