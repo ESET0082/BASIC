c1e56de5ac70dcd925ce8c51210be38acab779e8
ESET0082 <gopalbhai.bhammar@esyasoft.com>
2025-09-23 16:20:27 +0530

    add day-7 work

---
+++ Dot-Net/Day-7/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private List<Dictionary<string, string>> student_dict = new List<Dictionary<string, string>>
+    {
+        new Dictionary<string, string>{{"id","1"},{"name","Sachin"}},
+        new Dictionary<string, string>{{"id","2"},{"name","Rahul"}},
+    };
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            return new JsonResult(student_dict);
+        }
+        [HttpGet("{name}")]
+        public IActionResult GetStudentById(string id)
+        {
+            var student_details = student_dict.FirstOrDefault(p => p["id"] == id);
+            if(student_details != null)
+            {
+                return new JsonResult(student_details);
+            }
+            return new JsonResult("No machong record found");
+        }
+        public class ApiController1 : Controller
+        {
+            public IActionResult Index()
+            {
+                return View();
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
