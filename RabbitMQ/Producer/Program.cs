53bfe9676f943deb864ee3ec72215b2a5b05c672
ESET0082 <gopalbhai.bhammar@esyasoft.com>
2025-09-25 21:53:32 +0530

    update all folder

---
+++ RabbitMQ/Producer/Program.cs
@@ -0,0 +1,20 @@
+﻿using Producer.Exchanges;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost", VirtualHost = "smart_dev", UserName = "smart-project", Password = "1234" };
+
+TopicExchange directExchange = new TopicExchange(factory);
+FanoutExchange fanoutExchange = new FanoutExchange(factory);
+HeaderExchange headerExchange = new HeaderExchange(factory);
+Console.WriteLine("Enter messages to send (type 'exit' to quit):");
+
+while (true)
+{
+    string? input = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(input)) continue;
+    if (input.ToLower() == "exit") break;
+
+    await directExchange.SendMessage(input);
+
+
+}
